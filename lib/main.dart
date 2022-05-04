df5a63ac0a2724b3af4a3d94156083348516d5e6
Hakimi Mdnoor <hakimimdnoor90@gmail.com>
2022-05-04 20:14:04 +0800

    adjust for mobile app android

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:portfolio_hakimi/providers/home.provider.dart';
 import 'package:portfolio_hakimi/screen/splash.screen.dart';
 import 'package:provider/provider.dart';
@@ -7,14 +9,16 @@ import 'package:provider/provider.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-        apiKey: "AIzaSyDawhAuDD5hvGc9u-9Gj4rfKh-_gCHXjIA",
-        authDomain: "portfoliohakimi-b90b8.firebaseapp.com",
-        projectId: "portfoliohakimi-b90b8",
-        storageBucket: "portfoliohakimi-b90b8.appspot.com",
-        messagingSenderId: "562408540515",
-        appId: "1:562408540515:web:6aabd229eabd49b871ab56",
-        measurementId: "G-0FRCCZY4JV"),
+    options: kIsWeb == true
+        ? const FirebaseOptions(
+            apiKey: "AIzaSyDawhAuDD5hvGc9u-9Gj4rfKh-_gCHXjIA",
+            authDomain: "portfoliohakimi-b90b8.firebaseapp.com",
+            projectId: "portfoliohakimi-b90b8",
+            storageBucket: "portfoliohakimi-b90b8.appspot.com",
+            messagingSenderId: "562408540515",
+            appId: "1:562408540515:web:6aabd229eabd49b871ab56",
+            measurementId: "G-0FRCCZY4JV")
+        : null,
   );
   runApp(const MyApp());
 }
